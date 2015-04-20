828db3ccbd3d31499f0fdafd8957a0c630e25bf2
Victor Nguyen <vic.nguyen@gmail.com>
2015-04-20 21:09:55 +1000

    move peeps data to separate JSON file

--- app/scripts/app.jsx
+++ app/scripts/app.jsx
@@ -2,37 +2,9 @@
 
 var React       = window.React = require('react'),
     Header      = require('./ui/header.jsx'),
-    RegularList = require('./ui/regular_list.jsx');
+    RegularList = require('./ui/regular_list.jsx'),
+    PEEPS       = require('./peeps');
 
-
-var PEEPS = [
-    {
-        id: 1,
-        name: 'Vic',
-        count: 3,
-        order: {
-            type: 'Long Black',
-            sugar: 0,
-            strength: 'Normal',
-            notes: 'With a hint of morning mist. What.'
-        },
-        time: new Date().getHours() + '+' + new Date().getHours(),
-        lastVisited: null
-    },
-    {
-        id: 2,
-        name: 'Lars',
-        count: 6,
-        order: {
-            type: 'Espresso Martini',
-            sugar: 3,
-            strength: 'Strong',
-            notes: ''
-        },
-        time: new Date().getHours() + '+' + new Date().getHours(),
-        lastVisited: null
-    }
-];
 
 var App = React.createClass({
     getInitialState() {
