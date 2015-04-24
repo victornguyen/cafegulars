805d5d08a8d0c69ab5b4414cbbbd31a0f55225c4
Victor Nguyen <vic.nguyen@gmail.com>
2015-04-24 20:27:11 +1000

    added Add Person area

---
+++ app/scripts/ui/add_regular.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+
+var AddRegular = React.createClass({
+    render() {
+        return (
+            <div>
+                <h2>Add New Regular</h2>
+                <div className="panel panel-primary">
+                    <div className="panel-heading">
+                        <h3 className="panel-title">
+                            Name field
+                        </h3>
+
+                    </div>
+                    <div className="panel-body">
+                        Add form goes here man
+                    </div>
+                </div>
+            </div>
+        )
+    }
+});
+
+module.exports = AddRegular;
