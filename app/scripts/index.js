74bf812837929ff52b16dd82c9a9feec2fb416fa
Victor <vic.nguyen@gmail.com>
2015-08-26 00:34:55 +1000

    Make webpack bundle scss, bootstrap-sass, fonts

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,5 +2,6 @@
 
 import React from 'react';
 import App from './components/App';
+require('../styles/main.scss');
 
 React.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
+var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'app/scripts/index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist/scripts/'),
+    path: path.resolve(__dirname, 'public/scripts/'),
     publicPath: '/scripts/',
     filename: 'index.js'
   },
@@ -21,7 +22,24 @@ module.exports = {
       {
         test: /\.scss$/,
         loader: 'style!css!sass'
-      }
+      },
+
+      { test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+
+      { test: require.resolve('react'), loader: 'expose?React' }
     ]
-  }
+  },
+  resolve: {
+    root: [
+      path.join(__dirname, 'app/bower_components')
+    ]
+  },
+  plugins: [
+    new webpack.ResolverPlugin(
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
+    )
+  ]
 };
