548136a98a22b23fdc5e4c6c12136c1187a9799a
Victor Nguyen <vic.nguyen@gmail.com>
2015-11-17 17:55:32 +1100

    Remove old Regular flux references

--- src/scripts/components/RegularInfo.js
+++ src/scripts/components/RegularInfo.js
@@ -1,39 +1,8 @@
 'use strict';
 
-import React, { Component, PropTypes } from 'react';
-
-import RegularName       from 'components/RegularName';
-import RegularOrder      from 'components/RegularOrder';
-import RegularSugar      from 'components/RegularSugar';
-import RegularStrength   from 'components/RegularStrength';
-import RegularCounter    from 'components/RegularCounter';
-import RegularActions    from 'actions/RegularActions';
+import React, { Component } from 'react';
 
 class RegularInfo extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    // _addCup = () => {
-    //     RegularActions.addCup(this.props.regular.id);
-    // }
-
-    // _addFreeCup = () => {
-    //     RegularActions.addFreeCup(this.props.regular.id);
-    // }
-
-    // _updateSugar = (newCount) => {
-    //     RegularActions.updateSugar(this.props.regular.id, newCount);
-    // }
-
-    // _updateStrength = (strength) => {
-    //     RegularActions.updateStrength(this.props.regular.id, strength);
-    // }
-
-    // _updateOrder = (order) => {
-    //     RegularActions.updateOrder(this.props.regular.id, order);
-    // }
-
     render() {
         return (
             <div className="panel-body">
