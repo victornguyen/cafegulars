ba546932fe238bc2234ce0a496f1ec884db9d692
Victor Nguyen <vic.nguyen@gmail.com>
2015-12-01 23:31:32 +1100

    Refactor sugar component (cosmetic)

--- src/scripts/components/RegularSugar.js
+++ src/scripts/components/RegularSugar.js
@@ -4,41 +4,46 @@ import { MAX_SUGAR } from 'constants/Settings';
 
 import 'styles/regular-sugar';
 
+// TODO: make these SVGs
+const SugarIcon = () => (
+  <span
+    className="regular-sugar__icon glyphicon glyphicon-tint"
+    aria-hidden="true"
+  />
+);
+
 class RegularSugar extends Component {
-    static propTypes = {
-        count:          PropTypes.number.isRequired,
-        updateSugar:    PropTypes.func.isRequired
-    }
+  static propTypes = {
+    count:       PropTypes.number.isRequired,
+    updateSugar: PropTypes.func.isRequired
+  }
 
-    constructor(props) {
-        super(props);
-    }
+  handleClick = () => {
+    const { count, updateSugar } = this.props;
+    // TODO: should this logic happen here? or in Action Creator? or in Reducer?
+    const newCount = (count + 1 > MAX_SUGAR) ? 0 : count + 1;
+    updateSugar(newCount);
+  }
 
-    handleClick = () => {
-        // TODO: should this happen here? or in Action Creator? or in Reducer?
-        const count = (this.props.count + 1 > MAX_SUGAR) ? 0 : this.props.count + 1;
-        this.props.updateSugar(count);
-    }
+  render() {
+    let { count } = this.props;
+    return (
+      <button className="regular-sugar pull-left" onClick={this.handleClick}>
+        <div className="regular-sugar__count">
+          {count}
+        </div>
+        <div className="regular-sugar__icons">
+          {this.renderSugarIcons(count)}
+        </div>
+      </button>
+    );
+  }
 
-    render() {
-        return (
-            <button className="regular-sugar pull-left" onClick={this.handleClick}>
-                <div className="regular-sugar__count">
-                    {this.props.count}
-                </div>
-                <div className="regular-sugar__icons">
-                    {this.renderSugarIcons(this.props.count)}
-                </div>
-            </button>
-        );
-    }
-
-    renderSugarIcons(count) {
-        if (count === 0) {
-            return 'No Sugar';
-        }
-        return times(count, n => <span className="regular-sugar__icon glyphicon glyphicon-tint" aria-hidden="true" key={n}></span>);
-    }
+  renderSugarIcons(count) {
+    return count === 0
+      ? 'No Sugar'
+      : times(count, n => <SugarIcon key={n} />);
+  }
 }
 
 export default RegularSugar;
